b167315df1c40f28531ad2a6851a8ac7d3d132bd
fahrikurniawan99 <fahrikurniawan954@gmal.com>
2022-08-25 13:27:54 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Down from "./components/Down";
 function App() {
   return ( 
     <div className="bg-slate-200 container p-2">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, ducimus aut vero hic ipsum blanditiis similique consectetur voluptas accusamus nisi nulla repudiandae quasi sed aliquid, eos reiciendis dolorem cumque rem voluptatum maiores minima? Deleniti nisi voluptatibus eius consectetur explicabo quaerat vero tempora tempore odit laborum enim ad nemo repellat dolorem commodi dolor cumque reiciendis sit unde animi, corporis totam, ratione sint voluptas. Quas aspernatur sit porro ipsam atque consequatur reprehenderit ut error voluptate sint ipsum quis accusantium natus nisi quod maiores ea tempora suscipit iste voluptates quos, adipisci incidunt pariatur itaque. Optio, quia, similique hic laudantium odit perspiciatis ducimus adipisci atque rerum a asperiores eum minus reprehenderit in. Vel nemo sed aperiam, fugit ab eos magnam rerum voluptatibus dolores. Nulla architecto perferendis iusto reiciendis, aperiam perspiciatis ex, repudiandae, iste cumque praesentium voluptatibus quo amet labore a hic porro eos fugit in facilis. Obcaecati excepturi quas voluptas ad, neque sequi, placeat quasi sed inventore explicabo nisi. Quas eaque, dolorum dolore, quae molestias sit vitae accusamus dolores cumque consequatur deserunt aut molestiae iusto voluptatem libero ab, voluptatum ducimus. Eveniet quibusdam tempora autem illum natus at quaerat doloribus eius, alias delectus pariatur vitae, corrupti ab, velit eos repudiandae. Tenetur voluptas, quo nulla ex itaque explicabo facilis quae enim veritatisasas
+      <h1>akslaskslalka</h1>
     </div>
   );
 }
